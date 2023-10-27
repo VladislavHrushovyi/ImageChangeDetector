3c47c5494fda3e7b5c9e9b2fac39b6f0bdae8e9f
vladg <vlad.grush.99@gmail.com>
2023-10-27 15:53:45 +0300

    create new project for experiments

---
+++ Experiments/BitmapExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Experiments;
+
+public static class BitmapExtension
+{
+    public static IEnumerable<int> ReadAsBytesStream(this Bitmap bitmap)
+    {
+        using (MemoryStream stream = new MemoryStream())
+        {
+            bitmap.Save(stream, ImageFormat.Png);
+            using (BinaryReader binaryReader = new BinaryReader(stream))
+            {
+                while (stream.Position < stream.Length)
+                {
+                    yield return binaryReader.Read();
+                }
+            }
+        }
+    }
+}
---
+++ Experiments/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Drawing;
+using Experiments;
+
+var image = (Bitmap)Image.FromFile("./images/1.png");
+var imageBytes = image.ReadAsBytesStream();
+Console.WriteLine(string.Join("-",imageBytes.Take(4)));
+Console.Read();
