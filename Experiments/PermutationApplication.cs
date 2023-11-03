7837b34f61557a9488877576a4f990c5a8579250
vladg <vlad.grush.99@gmail.com>
2023-11-03 12:54:37 +0200

    extract permutation to app class

---
+++ Experiments/PermutationApplication.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using ImageChangeDetector;
+
+namespace Experiments;
+
+public class PermutationApplication
+{
+    private Bitmap Image;
+
+    public PermutationApplication(Bitmap image)
+    {
+        Image = image;
+    }
+
+    public PermutationApplication ColumnPermutation()
+    {
+        var colPermutation = new ColumnPermutation(new MatrixAccessor(Image.AsMatrix()));
+        Image = colPermutation.Execute();
+        return new PermutationApplication(Image);
+    }
+
+    public PermutationApplication RowPermutation()
+    {
+        var rowPermutation = new RowPermutation(new MatrixAccessor(Image.AsMatrix()));
+        Image = rowPermutation.Execute();
+
+        return new PermutationApplication(Image);
+    }
+
+    public void SaveImage(string path)
+    {
+        Image.Save(path);
+    }
+}
--- Experiments/Program.cs
+++ Experiments/Program.cs
@@ -2,12 +2,11 @@
 
 using System.Drawing;
 using Experiments;
-using ImageChangeDetector;
 
 var image = (Bitmap)Image.FromFile("./images/1.png");
-var imageSeparator = new ColumnPermutation(new MatrixAccessor(image.AsMatrix()));
-
-var result = imageSeparator.Execute();
-var rowSeparator = new RowPermutation(new MatrixAccessor(result.AsMatrix()));
-result = rowSeparator.Execute();
-result.Save("./images/result.png");
+var app = new PermutationApplication(image);
+app = app.ColumnPermutation()
+    .RowPermutation()
+    .ColumnPermutation()
+    .RowPermutation();
+app.SaveImage("./images/result.png");
